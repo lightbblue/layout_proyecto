16ebcf25b6e856369fa0b9f14d1de9dd928d4b02
lightbblue <jstachioni@gmail.com>
2024-08-08 17:18:10 -0300

    Feat #6 -Cambios en vista city.html

--- js/city.js
+++ js/city.js
@@ -1,13 +1,33 @@
 let local = localStorage.getItem("nombre");
+let container = document.querySelector("body");
 
+console.log(local)
 fetch("../json/cities.json")
 .then(response => {
     return response.json();
 })
 .then(data =>{
     data.forEach(e => {
-        (e.id==local)?`
-        
+        (e.name==local)?
+        container.innerHTML+=/*html*/`
+        <header>
+            <img src="${e.img}">
+                <a href="/html/main.html" class="fa-solid fa-circle-chevron-left back"></a>
+            <div class="like"><i class="fa-solid fa-heart"></i></div>
+        </header>
+        <main>
+            <div class="info">
+                <div class="title">${e.name} Tour</div>
+                <nav class="select">
+                    <a href="">Overview</a>
+                    <a href="">Reviews</a>
+                </nav>
+            </div>
+        </main>
+        <footer>
+
+        </footer>
+        <script src="../js/city.js"></script>
         `:0
     });
 })
