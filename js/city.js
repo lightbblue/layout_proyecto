d6e312f0e209572bb6a13a5f40e7d269ebe00c3b
ligthbblue <jstachioni@gmail.com>
2024-08-09 00:29:52 -0300

    Feat #7 -Fin city.html -Falta js y 5 vistas extra

--- js/city.js
+++ js/city.js
@@ -19,14 +19,35 @@ fetch("../json/cities.json")
             <div class="info">
                 <div class="title">${e.name} Tour</div>
                 <nav class="select">
-                    <a href="">Overview</a>
-                    <a href="">Reviews</a>
+                    <a class="selected" href="/overview">Overview</a>
+                    <a href="/reviews">Reviews</a>
                 </nav>
             </div>
+            <div class="ash">
+            <div class="info2">
+                <div class="elem">
+                    <div class="icon"><i class="fa-solid fa-star amarillo"></i><div class="dato">${e.rating}</div></div>
+                    <div class="tipo">Rating</div>
+                </div>
+                <div class="elem">
+                    <div class="icon"><i class="fa-solid fa-cloud celeste"></i></i><div class="dato">${e.temperature}°C</div></div>
+                    <div class="tipo">Duration</div>
+                </div>
+                <div class="elem">
+                    <div class="icon"><i class="fa-solid fa-clock naranja"></i></i><div class="dato">${e.duration} hrs</div></div>
+                    <div class="tipo">Duration</div>
+                </div>
+            </div>
+            <div class="desc">${e.description}</div>
         </main>
         <footer>
-
+            <div class="izq">
+                <div class="totalPrice">Total Price</div>
+                <div class="price">$${e.price}</div>
+            </div>
+            <a href="/html/main.html" class="der">Book Now</a>
         </footer>
+        </div>
         <script src="../js/city.js"></script>
         `:0
     });
